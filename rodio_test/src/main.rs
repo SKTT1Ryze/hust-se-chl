5eac65be4210f7f9bf60611db9ca18fc7dc202b0
hustccc <1276675421@qq.com>
2020-11-22 08:36:57 +0900

    change some rodio_test

--- rodio_test/src/main.rs
+++ rodio_test/src/main.rs
@@ -3,15 +3,24 @@
 use std::{
     fs::File,
     io::BufReader,
+    thread,
+    time::Duration,
 };
-use rodio::Source;
 
 fn main() {
     println!("Hello, ridio!");
-    let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-    let file = File::open("./mp3/CLICK.mp3").unwrap();
-    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-    stream_handle.play_raw(source.convert_samples());
-
-    loop {}
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+    let file_0 = File::open("./mp3/CLICK.mp3").unwrap();
+    let file_1 = File::open("./mp3/CLICK.mp3").unwrap();
+    let source_0 = rodio::Decoder::new(BufReader::new(file_0)).unwrap();
+    let source_1 = rodio::Decoder::new(BufReader::new(file_1)).unwrap();
+    sink.append(source_0);
+    sink.append(source_1);
+    sink.set_volume(0.2);
+    thread::sleep(Duration::from_millis(3000));
+    sink.pause();
+    thread::sleep(Duration::from_millis(3000));
+    sink.play();
+    sink.sleep_until_end();
 }
