4a62117e7445d02e66dec4b9b634ec92ad920efb
hustccc <1276675421@qq.com>
2020-11-22 19:59:24 +0900

    fix something

--- rodio_test/src/main.rs
+++ rodio_test/src/main.rs
@@ -6,17 +6,21 @@ use std::{
     thread,
     time::Duration,
 };
-
+use rodio::Source;
 fn main() {
     println!("Hello, ridio!");
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&stream_handle).unwrap();
-    let file_0 = File::open("./mp3/CLICK.mp3").unwrap();
-    let file_1 = File::open("./mp3/CLICK.mp3").unwrap();
-    let source_0 = rodio::Decoder::new(BufReader::new(file_0)).unwrap();
-    let source_1 = rodio::Decoder::new(BufReader::new(file_1)).unwrap();
-    sink.append(source_0);
-    sink.append(source_1);
+    let file = File::open("./mp3/CLICK.mp3").unwrap();
+    let data_0 = BufReader::new(file.try_clone().unwrap());
+    // let data_1 = BufReader::new(file.try_clone().unwrap());
+    let source = rodio::Decoder::new(data_0).unwrap().buffered();
+    // let source_0 = rodio::Decoder::new(data_0).unwrap();
+    // let source_1 = rodio::Decoder::new(data_1).unwrap();
+    // sink.append(source_0);
+    // sink.append(source_1);
+    sink.append(source.clone());
+    sink.append(source.clone());
     sink.set_volume(0.2);
     thread::sleep(Duration::from_millis(3000));
     sink.pause();
